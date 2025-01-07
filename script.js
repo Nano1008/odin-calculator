7485311b89924bf7b03e6f430fbddf62057c50fe
Sinan <nanossn1008@gmail.com>
2025-01-07 20:23:14 +0000

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
     return a / b;
 }
+
+let num1;
+let num2;
+let operator;
+
+const operate = function (operator, operand1, operand2) {
+    switch (operator) {
+        case "+":
+            return add(operand1, operand2)
+            break;
+        case "-":
+            return subtract(operand1, operand2);
+            break;
+        case "*":
+            return multiply(operand1, operand2);
+            break;
+        case "/":
+            return divide(operand1, operand2);
+            break;
+        default:
+            return "Not a valid operator!";
+            break;
+    }
+}
