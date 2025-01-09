2296a7773273e842b6f257f024cb7b100f2ecd19
Sinan <nanossn1008@gmail.com>
2025-01-09 19:14:57 +0000

    Finish calculator

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
-const add = function (a, b) {
-    return a + b;
+// Basic operations
+const operations = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+    modulo: (a, b) => a % b,
 }
 
-const subtract = function (a, b) {
-    return a - b;
-}
-
-const multiply = function (a, b) {
-    return a * b;
-}
-
-const divide = function (a, b) {
-    return a / b;
-}
-
-const modulo = function (a, b) {
-    return a % b;
-}
-
-const operate = function (operator, operand1, operand2) {
+const operate = (operator, operand1, operand2) => {
     switch (operator) {
         case "+":
-            return add(operand1, operand2)
+            return operations.add(operand1, operand2);
             break;
         case "-":
-            return subtract(operand1, operand2);
+            return operations.subtract(operand1, operand2);
             break;
         case "*":
-            return multiply(operand1, operand2);
+            return operations.multiply(operand1, operand2);
             break;
         case "/":
-            return divide(operand1, operand2);
+            return operations.divide(operand1, operand2);
             break;
         case "%":
-            return modulo(operand1, operand2);
+            return operations.modulo(operand1, operand2);
             break;
         default:
             return "Not a valid operator!";
@@ -41,27 +30,76 @@ const operate = function (operator, operand1, operand2) {
     }
 }
 
-const buttons = document.querySelectorAll("button");
-let display = document.querySelector("#display");
-let expression = "";
+class Calculator {
+    constructor(displayElement) {
+        this.display = displayElement;
+        this.expression = "";
+    }
 
-buttons.forEach((button) => {
-    button.addEventListener("click", e => {
-        const value = e.target.textContent;
+    isOperator(char) {
+        return ['+', '-', '*', '/', '%'].includes(char);
+    }
 
-        if (value === "C") {
-            expression = "";
-            display.value = expression;
-        } else if (value === "=") {
-            const operand1 = parseInt(expression.charAt(0));
-            const operator = expression.charAt(1);
-            const operand2 = parseInt(expression.charAt(2));
-            const result = operate(operator, operand1, operand2);
-            display.value = result;
+    hasCompleteExpression(expr) {
+        return expr.split(/[\+\-\*\/\%]/).length >= 2;
+    }
 
-        } else {
-            expression += value;
-            display.value = expression;
+    updateDisplay() {
+        this.display.value = this.expression;
+    }
+
+    evaluateExpression(expr) {
+        const numbers = expr.split(/[\+\-\*\/\%]/).map(num => parseInt(num));
+        const operators = expr.split("").filter(char => this.isOperator(char));
+
+        let result = numbers[0];
+        for (let i = 0; i < operators.length; i++) {
+            result = operate(operators[i], result, numbers[i + 1]);
         }
+        return result;
+    }
+
+    clear() {
+        this.expression = "";
+        this.updateDisplay();
+    }
+
+    calculate() {
+        const result = this.evaluateExpression(this.expression);
+        this.expression = String(result);
+        this.updateDisplay();
+    }
+
+    updateExpression(val) {
+        if (this.isOperator(val) && this.hasCompleteExpression(this.expression)) {
+            const result = this.evaluateExpression(this.expression);
+            this.expression = result + val;
+        } else {
+            this.expression += val;
+        }
+        this.updateDisplay();
+    }
+
+    handleInput(val) {
+        if (val === 'C') {
+            this.clear();
+        } else if (val === "=") {
+            this.calculate();
+        } else {
+            this.updateExpression(val);
+        }
+    }
+}
+
+function initCalculator() {
+    const display = document.querySelector("#display");
+    const calc = new Calculator(display);
+
+    document.querySelectorAll("button").forEach(button => {
+        button.addEventListener("click", e => {
+            calc.handleInput(e.target.textContent);
+        })
     })
-});
+}
+
+document.addEventListener('DOMContentLoaded', initCalculator);
